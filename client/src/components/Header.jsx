2830f51b53b6e2d874166917713018bf911b4132
JaysonDorilag22 <jaysondorilag1@gmail.com>
2023-11-03 22:58:47 +0800

    full update functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -13,185 +13,54 @@ export default function Header() {
         <Link to={"/"}>
           <img src={logo} alt="Logo" className="h-10 w-15" />
         </Link>
-        <div className="flex items-center"> {/* Wrap avatar and cart in a flex container */}
-          <div className="dropdown dropdown-end ml-2"> {/* Cart dropdown */}
-            <label tabIndex={0} className="btn btn-ghost btn-circle">
-              <div className="indicator">
-                <FiShoppingCart className="h-5 w-5" />
-                <span className="badge badge-sm indicator-item">8</span>
-              </div>
-            </label>
-            <div className="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow">
-              <div className="card-body">
-                <span className="font-bold text-lg">8 Items</span>
-                <span className="text text-green-600">Subtotal: $999</span>
-                <div className="card-actions">
-                  <button className="btn  btn-outline">View cart</button>
+        {currentUser ? (
+          <div className="flex items-center">
+            <div className="dropdown dropdown-end ml-2"> {/* Cart dropdown */}
+              <label tabIndex={0} className="btn btn-ghost btn-circle">
+                <div className="indicator">
+                  <FiShoppingCart className="h-5 w-5" />
+                  <span className="badge badge-sm indicator-item">8</span>
+                </div>
+              </label>
+              <div className="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow">
+                <div className="card-body">
+                  <span className="font-bold text-lg">8 Items</span>
+                  <span className="text text-green-600">Subtotal: $999</span>
+                  <div className="card-actions">
+                    <button className="btn btn-outline">View cart</button>
+                  </div>
                 </div>
               </div>
             </div>
+            <div className="dropdown dropdown-end ml-5"> {/* Avatar dropdown */}
+              <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                {currentUser ? (
+                  <div className="w-10 rounded-full">
+                    <img src={currentUser.avatar} alt="profile" />
+                  </div>
+                ) : (
+                  <li className=" text-slate-700 hover:underline"> Sign in</li>
+                )}
+              </label>
+              <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+                <li>
+                  <Link to="/profile" className="justify-between">
+                    Profile
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/settings">Orders
+                  <div className="badge badge-error bg-red-600 text-white badge-sm">34</div>
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/logout">Logout</Link>
+                </li>
+              </ul>
+            </div>
           </div>
-          <div className="dropdown dropdown-end ml-5"> {/* Avatar dropdown */}
-            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-              {currentUser ? (
-                <div className="w-10 rounded-full">
-                  <img src={currentUser.avatar} alt="profile" />
-                </div>
-              ) : (
-                <li className=" text-slate-700 hover:underline"> Sign in</li>
-              )}
-            </label>
-            <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
-              <li>
-                <Link to="/profile" className="justify-between">
-                  Profile
-                </Link>
-              </li>
-              <li>
-                <Link to="/settings">Orders
-                <div className="badge badge-error bg-red-600 text-white badge-sm">34</div>
-                </Link>
-              </li>
-              <li>
-                <Link to="/logout">Logout</Link>
-              </li>
-            </ul>
-          </div>
-        </div>
+        ) : null}
       </div>
     </header>
   );
 }
-
-// import { FaSearch } from "react-icons/fa";
-// import { Link } from "react-router-dom";
-// import { useSelector } from "react-redux";
-// import logo from "../assets/images/logo.png";
-
-// export default function Header() {
-//   const { currentUser } = useSelector((state) => state.user);
-
-//   return (
-//     <header className="bg-white-200 shadow-md">
-//       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
-//         <Link to={"/"}>
-//           <img src={logo} alt="Logo" className="h-10 w-15" />
-//         </Link>
-//         <form className="bg-slate-100 p-3 rounded-lg flex items-center">
-//           <input
-//             type="text"
-//             placeholder="Search..."
-//             className="bg-transparent focus:outline-none w-24 sm:w-64"
-//           />
-//           <FaSearch className="text-slate-700 " />
-//         </form>
-//         <div className="dropdown dropdown-end">
-//           <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-//             {currentUser ? (
-//               <div className="w-10 rounded-full">
-//                 <img src={currentUser.avatar} alt="profile" />
-//               </div>
-//             ) : (
-//               <li className=" text-slate-700 hover:underline"> Sign in</li>
-//             )}
-//           </label>
-//           <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
-//             <li>
-//               <Link to="/profile" className="justify-between">
-//                 Profile
-//               </Link>
-//             </li>
-//             <li>
-//               <Link to="/settings">Cart
-//               <span className="badge bg-red-600 text-white">30</span>
-//               </Link>
-//             </li>
-//             <li>
-//               <Link to="/signout">Sign out</Link>
-//             </li>
-//           </ul>
-//         </div>
-//       </div>
-//     </header>
-//   );
-// }
-
-
-// import React from "react";
-// import logo from "../assets/images/logo.png";
-// import { Link } from "react-router-dom";
-// import { useSelector } from "react-redux";
-// import { BiSearch, BiCart } from "react-icons/bi"; // Import BiCart
-
-// export default function Header() {
-//   const { currentUser } = useSelector((state) => state.user);
-
-//   return (
-//     <div>
-//       <div className="navbar bg-base-100">
-//         <div className="flex-1">
-//           <Link to={"/"}>
-//             <img src={logo} alt="Logo" className="h-10 w-15" />
-//           </Link>
-//         </div>
-//         <div className="flex-none">
-//           <div className="dropdown dropdown-end">
-//             <label tabIndex={0} className="btn btn-ghost btn-circle">
-//               <div className="indicator">
-//                 <span className="badge badge-sm indicator-item">8</span>
-//               </div>
-//             </label>
-//             <div
-//               tabIndex={0}
-//               className="mt-3 z-10 card card-compact dropdown-content w-52 bg-base-100 shadow"
-//             >
-//               <div className="card-body">
-//                 <span className="font-bold text-lg">
-//                   <BiSearch /> 8 Items
-//                 </span>
-//                 <span className="text-info">Subtotal: $999</span>
-//                 <div className="card-actions">
-//                   <button className="btn btn-primary btn-block">
-//                     <BiCart /> View cart
-//                   </button>
-//                 </div>
-//               </div>
-//             </div>
-//           </div>
-//           <div className="dropdown dropdown-end">
-//             <label tabIndex={0} className="btn btn-ghost btn-circle avatar hidden">
-//               <Link to="/profile">
-//                 {currentUser ? (
-//                   <img
-//                     className="rounded-full h-7 w-7 object-cover"
-//                     src={currentUser.avatar}
-//                     alt="profile"
-//                   />
-//                 ) : (
-//                   <span className="text-slate-700 hover:underline">Sign in</span>
-//                 )}
-//               </Link>
-//             </label>
-//             <ul
-//               tabIndex={0}
-//               className="menu menu-sm dropdown-content mt-3 z-10 p-2 shadow bg-base-100 rounded-box w-52"
-//             >
-//               <li>
-//                 <a className="justify-between">
-//                   Profile
-//                   <span className="badge">New</span>
-//                 </a>
-//               </li>
-//               <li>
-//                 <a>Settings</a>
-//               </li>
-//               <li>
-//                 <a>Logout</a>
-//               </li>
-//             </ul>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
--- client/src/components/user/Profile.jsx
+++ client/src/components/user/Profile.jsx
@@ -15,13 +15,12 @@ import {
   deleteUserStart,
   deleteUserSuccess,
   // signOutUserStart,
-} from '../../redux/user/userSlice.js';
-import { useDispatch } from 'react-redux';
-
+} from "../../redux/user/userSlice.js";
+import { useDispatch } from "react-redux";
 
 export default function Profile() {
   const fileRef = useRef(null);
-  const { currentUser } = useSelector((state) => state.user);
+  const { currentUser, loading, error } = useSelector((state) => state.user);
   const [file, setFile] = useState(undefined);
   const [filePerc, setFilePerc] = useState(0);
   const [fileUploadError, setFileUploadError] = useState(false);
@@ -32,7 +31,6 @@ export default function Profile() {
   const dispatch = useDispatch();
 
   console.log(formData);
-
 
   useEffect(() => {
     if (file) {
@@ -68,7 +66,7 @@ export default function Profile() {
     try {
       dispatch(deleteUserStart());
       const res = await fetch(`/api/user/delete/${currentUser._id}`, {
-        method: 'DELETE',
+        method: "DELETE",
       });
       const data = await res.json();
       if (data.success === false) {
@@ -81,10 +79,37 @@ export default function Profile() {
     }
   };
 
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(updateUserStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(updateUserFailure(data.message));
+        return;
+      }
+      dispatch(updateUserSuccess(data));
+      setUpdateSuccess(true);
+    } catch (error) {
+      dispatch(updateUserFailure(error.message));
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
-      <form className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           onChange={(e) => setFile(e.target.files[0])}
           type="file"
@@ -98,36 +123,47 @@ export default function Profile() {
           alt="profile"
           className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
         />
-        <p className='text-sm self-center'>
+        <p className="text-sm self-center">
           {fileUploadError ? (
-            <span className='text-red-700'>
+            <span className="text-red-700">
               Error Image upload (image must be less than 2 mb)
             </span>
           ) : filePerc > 0 && filePerc < 100 ? (
-            <span className='text-slate-700'>{`Uploading ${filePerc}%`}</span>
+            <span className="text-slate-700">{`Uploading ${filePerc}%`}</span>
           ) : filePerc === 100 ? (
-            <span className='text-green-700'>Image successfully uploaded!</span>
+            <span className="text-green-700">Image successfully uploaded!</span>
           ) : (
-            ''
+            ""
           )}
         </p>
         <input
           type="text"
           placeholder="username"
+          id="username"
+          defaultValue={currentUser.username}
           className="border p-3 rounded-lg id='username"
+          onChange={handleChange}
         />
         <input
           type="email"
           placeholder="email"
+          id="email"
+          defaultValue={currentUser.email}
           className="border p-3 rounded-lg id='email"
+          onChange={handleChange}
         />
         <input
-          type="text"
+          type="password"
+          id="password"
           placeholder="password"
           className="border p-3 rounded-lg id='password"
+          onChange={handleChange}
         />
-        <button className="bg-slate-700  text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">
-          Update
+        <button
+          disabled={loading}
+          className="bg-slate-700  text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "loading..." : "update"}
         </button>
       </form>
       <div className="flex justify-between mt-5">
@@ -139,6 +175,10 @@ export default function Profile() {
         </span>
         <span className="text-red-700 cursor-pointer">Sign out</span>
       </div>
+      <p className="text-red-700 mt-5">{error ? error : ""}</p>
+      <p className="text-green-700 mt-5">
+        {updateSuccess ? "User is updated successfully!" : ""}
+      </p>
     </div>
   );
 }
