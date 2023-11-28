6c54ce266c9dad7853a5b561f2d8e07dec3cb016
JaysonDorilag22 <jaysondorilag1@gmail.com>
2023-11-28 20:50:41 +0800

    fix issue

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,7 +5,7 @@ import slide2 from "../assets/images/slide2.jpg";
 import slide3 from "../assets/images/slide3.jpg";
 import hero from "../assets/images/hero.jpg";
 import home from "../assets/images/home.jpg";
-import CategoryCards from "../components/ecommerce/Category/CategoryCard";
+import CategoryCards from "../components/ecommerce/Categories/CategoryCard";
 import ProductCards from "../components/ecommerce/Product/ProductCard";
 
 export default function Home() {
